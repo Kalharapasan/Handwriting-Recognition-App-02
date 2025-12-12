37e7ca23c6a05e41a032a39063b9970ef9b761e5
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-12-12 22:45:39 +0530

    Predict multiple digits

--- utils.py
+++ utils.py
@@ -8,3 +8,12 @@ from pdf2image import convert_from_path
 import tempfile
 
 class ModelManager:
+    
+    
+    def predict_multiple_digits(self, images):
+        results = []
+        for image in images:
+            processed_image = ImagePreprocessor.preprocess_image(image)
+            digit, confidence = self.predict_digit(processed_image)
+            results.append((digit, confidence))
+        return results
