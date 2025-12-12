0e27629d63084f36bc4e0fe10e05667dc993e2f3
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-12-12 22:34:32 +0530

    create class PredictionHistory

--- database.py
+++ database.py
@@ -7,3 +7,14 @@ from datetime import datetime
 import os
 
 Base = declarative_base()
+
+class PredictionHistory(Base):
+    __tablename__ = 'prediction_history'
+    
+    id = Column(Integer, primary_key=True)
+    timestamp = Column(DateTime, default=datetime.utcnow)
+    predicted_digit = Column(Integer)
+    confidence = Column(Float)
+    image_path = Column(String(500))
+    user_input_type = Column(String(50))  
+    file_name = Column(String(255))
